739c5ecbf29525d3861326ea7bcfc9aae7ca3286
Andrew58555 <nikon.andrew228@gmail.com>
2025-09-09 10:48:06 +0300

    Update main3.cpp

--- semester_1/lab1_introduction/main3.cpp
+++ semester_1/lab1_introduction/main3.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
-#define int long long
 
-int32_t main()
+int main()
 {
     int n;
     std::cout << "Enter n : ";
-    std::cin >> n;
+    if (!(std::cin >> n)) {
+        std::cout << "Error!";
+        std::exit(1);
+    }
     int k = 0;
     for (int i = 1; i <= n; i++) {
         k += pow(i, i);
@@ -13,3 +15,4 @@ int32_t main()
     std::cout << k;
     return 0;
 }
+
