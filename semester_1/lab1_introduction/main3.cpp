00dbb38648637b87a555ef61114975232df33d9b
Andrew58555 <nikon.andrew228@gmail.com>
2025-09-12 14:02:29 +0300

    Update main3.cpp

--- semester_1/lab1_introduction/main3.cpp
+++ semester_1/lab1_introduction/main3.cpp
@@ -11,12 +11,13 @@ int main() {
     for (int i = 1; i <= n; i++) {
         k += pow(i, i);
     }
-    std::cout << k;
+    std::cout << "sum = " << k;
     return 0;
 }
 //if n == 5, then the answer will be 3413
 //if n == 6, then the answer will be 50069
 //if n == 10, then the answer will be 10405071317
 //if n > 15, then the answer will be outside the data type and output space debris
+
 
 
