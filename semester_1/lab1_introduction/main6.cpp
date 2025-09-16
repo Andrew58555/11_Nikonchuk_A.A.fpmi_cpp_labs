49cbf100deaf9f0f7a02f8b2320122a64d23d00a
Andrew58555 <nikon.andrew228@gmail.com>
2025-09-16 10:53:25 +0300

    Update main6.cpp

--- semester_1/lab1_introduction/main6.cpp
+++ semester_1/lab1_introduction/main6.cpp
@@ -1,11 +1,12 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 int main() {
+
     int n;
     std::cout << "Enter n : ";
     if (!(std::cin >> n) || n < 1) {
         std::cout << "Error!";
-        std::exit(1);
+        return 1;
     }
     int sum = 0, l = 1;
     for (int i = 1; i <= n; i++) {
@@ -15,6 +16,3 @@ int main() {
     std::cout << "sum = " << sum;
     return 0;
 }
-
-
-
