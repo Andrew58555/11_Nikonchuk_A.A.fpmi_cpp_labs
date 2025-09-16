2a7eaff0fde7fae34fad3c6ffa945155e6690f21
Andrew58555 <nikon.andrew228@gmail.com>
2025-09-16 10:48:34 +0300

    Update main4.cpp

--- semester_1/lab1_introduction/main4.cpp
+++ semester_1/lab1_introduction/main4.cpp
@@ -1,29 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 int main() {
+
     int k;
     std::cout << "Enter k : ";
     if (!(std::cin >> k) || k < 1) {
         std::cout << "Error!";
-        std::exit(1);
+        return 1;
     }
+    int subf = 1;
     if (k % 2 == 0) {
-        int f = 1;
         for (int i = 2; i <= k; i += 2) {
-            f *= i;
+            subf *= i;
         }
-        std::cout << "subfactorial = " << f;
     }
     else {
-        int f = 1;
         for (int i = 1; i <= k; i += 2) {
-            f *= i;
+            subf *= i;
         }
-        std::cout << "subfactorial = " << f;
     }
+    std::cout << "subfactorial = " << subf;
     return 0;
 }
-
-
-
-
